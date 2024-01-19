526445ee540ce3f22ba412150567ded460951260
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-19 10:27:18 +0100

    push requirements

---
+++ Installations.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Requirements and Packages to have in order for the game to work
+
+# Install Python
+https://www.python.org/downloads/
+
+# Update Python
+pip install --upgrade pip
+
+# install Pygame
+pip install Pygame
+
+# install TKinter
+pip install tk
